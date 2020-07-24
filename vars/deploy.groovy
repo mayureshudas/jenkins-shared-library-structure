a4a2f954a4b777b44d5e9c23108c1e8b6b398db2
mayureshudas <mayuresh.udas@capgemini.com>
2020-07-24 09:25:20 +0530

    Update deploy.groovy

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -1,7 +1,7 @@
 def call(Map config) {
        
        sh "cd /target"
-       sh "\cp -fR petclinic.war /var/lib/tomcat/webapps"
+       sh "cp -fR petclinic.war /var/lib/tomcat/webapps"
        sh "service tomcat restart"
 
   
